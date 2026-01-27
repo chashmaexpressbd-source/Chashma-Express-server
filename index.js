2a526921a0cb025999367a816db6a75289339875
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-01-27 21:14:05 +0600

    add cors

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const port = process.env.PORT || 5000;
 
 // ---------- Middleware ----------
 app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'https://chashma-express-client.vercel.app',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 // ---------- MongoDB Setup ----------
